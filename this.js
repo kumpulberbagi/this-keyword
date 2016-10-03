387b949004b72112bf931c7b47b3a3c0f99f6a7e
Aji Lantang Mardika & Mangku Widodo <pair+ajilantang.mangkuwi@devbootcamp.com>
2016-10-03 11:27:53 +0700

    tugas this.keyword

--- this.js
+++ this.js
@@ -1,11 +1,33 @@
 "use strict"
 
 // GLOBAL CONTEXT
+this.x = 37;
+var z = this.x
+console.log(this.x+4);//output 41
 
+function tambah(value){
+  return value+=z
+}
 
+console.log(tambah(5));//jika menggunakan var z = this.x maka keluar
 
-
+function tambah2(value){
+  return value+=this.x
+}
+//console.log(tambah2(5));//jika menggunakan this.x pada function porperty x undefined dan tidak bisa di eksekusi atau function eror
 // CLASS CONTEXT
-
-
-// RELEASE 1
+class hewan {
+  constructor (nama,makan,minum){
+    this.makan = makan;
+    this.minum = minum+ z;
+    this.nama = nama;
+  }
+}
+var anjing = new hewan("doggy", "kripik","mineral water")
+console.log(anjing.minum);
+// console.log(this.makan);
+ console.log(this.makan);//undefined
+// // RELEASE 1
+// pada global konteks kita bisa manggil this dimanapubn sebagai variabel global  akan tetapi harus di inisialisasi dengan var tertentu yang samadengan nilai this. jika this dipanggil langsung pada function
+//maka akan eror, this pada class hanya bisa diakses oleh clas itu sendiri . contoh this makan yg akan keluar undefined, akan tetapi
+// ketika dipanggil menggunakan class maka
